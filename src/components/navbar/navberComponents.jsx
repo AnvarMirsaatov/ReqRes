5689224aa59f955706c3130f3d77ba99610109f3
Anvar <158838160+AnvarMirsaatov@users.noreply.github.com>
2025-02-11 16:58:59 +0500

    Update navberComponents.jsx

--- src/components/navbar/navberComponents.jsx
+++ src/components/navbar/navberComponents.jsx
@@ -2,10 +2,9 @@ import Link from 'next/link'
 
 export default function NavbarComponents () {
   return (
-    <nav className='w-full bg-gray-500 py-2 shadow-lg  flex justify-center'>
+    <nav className='w-full bg-gray-300 py-2 shadow-lg  flex justify-center'>
 
       <Link href='/'>
-        {/* <span className='text-center text-3xl cursor-pointer'>ReqRes</span> */}
         <img className='h-10' src='https://reqres.in/img/logo.png' alt='' />
 
       </Link>
